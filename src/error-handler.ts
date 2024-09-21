dcce25dadab4ea358d79b34a104e1e8bbf334652
Gabriel Alves <gabriel52.alves@gmail.com>
2024-09-21 15:22:47 -0300

    Refactor error handling for ZodError and BadRequestException

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { ErrorCode, HttpException } from "./exceptions/root";
 import { InternalException } from "./exceptions/internal-exception";
+import { ZodError } from "zod";
+import { BadRequestException } from "./exceptions/bad-requests";
 
 export const errorHandler = (method: Function) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -18,11 +20,19 @@ export const errorHandler = (method: Function) => {
           error.issues
         );
       } else {
-        exception = new InternalException(
-          "Something went wrong",
-          error,
-          ErrorCode.INTERNAL_EXCEPTION
-        );
+        if (error instanceof ZodError) {
+          exception = new BadRequestException(
+            "Unprocessable Entity",
+            ErrorCode.UNPROCESSABLE_ENTITY,
+            error
+          );
+        } else {
+          exception = new InternalException(
+            "Something went wrong",
+            error,
+            ErrorCode.INTERNAL_EXCEPTION
+          );
+        }
       }
       next(exception);
     }
