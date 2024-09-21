ad93142723f352418c11c9fde68e3590a9dd9def
Gabriel Alves <gabriel52.alves@gmail.com>
2024-09-21 12:34:01 -0300

    Add createProduct route and middleware to restrict access to ADMIN users

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,4 +1,13 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
 
-export const createProduct = async (req: Request, res: Response) => {};
+export const createProduct = async (req: Request, res: Response) => {
+  const product = await prismaClient.product.create({
+    data: {
+      ...req.body,
+      tags: req.body.tags.join(","),
+    },
+  });
+
+  res.json(product);
+};
---
+++ src/middlewares/admin.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { UnauthorizedException } from "../exceptions/unauthorized";
+import { ErrorCode } from "../exceptions/root";
+
+const adminMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = req.user;
+  if (user.role == "ADMIN") {
+    next();
+  } else {
+    return next(
+      new UnauthorizedException(
+        "Token not found (Unauthorized)",
+        ErrorCode.UNAUTHORIZED
+      )
+    );
+  }
+};
+
+export default adminMiddleware;
