1f97e57b1eb03077de958e52ec6324f8e70cf2d3
Gabriel Alves <gabriel52.alves@gmail.com>
2024-09-21 15:04:51 -0300

    Add CRUD operations and error handling

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/root";
 
 export const createProduct = async (req: Request, res: Response) => {
   const product = await prismaClient.product.create({
@@ -10,4 +12,83 @@ export const createProduct = async (req: Request, res: Response) => {
   });
 
   res.json(product);
+};
+
+export const createProducts = async (req: Request, res: Response) => {
+  const productsData = req.body;
+  const createdProducts = [];
+
+  for (const productData of productsData) {
+    const { tags, ...product } = productData;
+    const createdProduct = await prismaClient.product.create({
+      data: {
+        ...product,
+        tags: tags.join(","),
+      },
+    });
+    createdProducts.push(createdProduct);
+  }
+
+  res.status(201).json(createdProducts);
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const product = req.body;
+    if (product.tags) {
+      product.tags = product.tags.join(",");
+    }
+
+    const updatedProduct = await prismaClient.product.update({
+      where: {
+        id: +req.params.id,
+      },
+      data: product,
+    });
+    res.json(updatedProduct);
+  } catch (err) {
+    throw new NotFoundException(
+      "Product not found",
+      ErrorCode.PRODUCT_NOT_FOUND
+    );
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    await prismaClient.product.delete({
+      where: {
+        id: +req.params.id,
+      },
+    });
+    res.json({ message: "Product deleted" });
+  } catch (err) {
+    throw new NotFoundException(
+      "Product not found",
+      ErrorCode.PRODUCT_NOT_FOUND
+    );
+  }
+};
+
+export const listProducts = async (req: Request, res: Response) => {
+  const count = await prismaClient.product.count();
+  const products = await prismaClient.product.findMany({
+    skip: +req.query.skip! || 0,
+    take: 5,
+  });
+  res.json({ count, data: products });
+};
+
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const product = await prismaClient.product.findFirstOrThrow({
+      where: { id: +req.params.id },
+    });
+    res.json(product);
+  } catch (error) {
+    throw new NotFoundException(
+      "Product not found",
+      ErrorCode.PRODUCT_NOT_FOUND
+    );
+  }
 };
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import { errorHandler } from "../error-handler";
-import { createProduct } from "../controllers/products";
+import {
+  createProduct,
+  createProducts,
+  deleteProduct,
+  getProductById,
+  listProducts,
+  updateProduct,
+} from "../controllers/products";
 import authMiddleware from "../middlewares/auth";
 import adminMiddleware from "../middlewares/admin";
 
@@ -10,6 +17,31 @@ productsRoutes.post(
   "/",
   [authMiddleware, adminMiddleware],
   errorHandler(createProduct)
+);
+productsRoutes.post(
+  "/multiple/",
+  [authMiddleware, adminMiddleware],
+  errorHandler(createProducts) // essa rota espera um array de produtos, ou seja, serve para criar multiplos produtos
+);
+productsRoutes.put(
+  "/:id",
+  [authMiddleware, adminMiddleware],
+  errorHandler(updateProduct)
+);
+productsRoutes.delete(
+  "/:id",
+  [authMiddleware, adminMiddleware],
+  errorHandler(deleteProduct)
+);
+productsRoutes.get(
+  "/",
+  [authMiddleware, adminMiddleware],
+  errorHandler(listProducts)
+);
+productsRoutes.get(
+  "/:id",
+  [authMiddleware, adminMiddleware],
+  errorHandler(getProductById)
 );
 
 export default productsRoutes;
