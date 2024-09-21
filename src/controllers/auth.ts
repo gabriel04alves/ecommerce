aecdeb492de901d30d60063573b5eccd1fc6e037
Gabriel Alves <gabriel52.alves@gmail.com>
2024-09-21 11:36:43 -0300

    Refactor signup controller, add validation and handle existing user error

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -14,40 +14,27 @@ export const signup = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    SignUpSchema.parse(req.body);
-    const { email, password, name } = req.body;
+  SignUpSchema.parse(req.body);
+  const { email, password, name } = req.body;
 
-    let user = await prismaClient.user.findFirst({ where: { email } });
-    if (user) {
-      return next(
-        new BadRequestException(
-          "User already exists",
-          ErrorCode.USER_ALREADY_EXISTS
-        )
-      );
-    }
-
-    user = await prismaClient.user.create({
-      data: {
-        name,
-        email,
-        password: hashSync(password, 10),
-      },
-    });
-    res.json(user);
-  } catch (err: any) {
-    if (err.name === "ZodError") {
-      return next(
-        new UnprocessableEntity(
-          err.errors,
-          "Invalid data provided",
-          ErrorCode.UNPROCESSABLE_ENTITY
-        )
-      );
-    }
-    next(err);
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (user) {
+    return next(
+      new BadRequestException(
+        "User already exists",
+        ErrorCode.USER_ALREADY_EXISTS
+      )
+    );
   }
+
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+    },
+  });
+  res.json(user);
 };
 
 export const login = async (
@@ -55,34 +42,31 @@ export const login = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    let user = await prismaClient.user.findFirst({ where: { email } });
-    if (!user) {
-      return next(
-        new NotFoundException("User not found", ErrorCode.USER_NOT_FOUND)
-      );
-    }
-
-    if (!compareSync(password, user.password)) {
-      return next(
-        new BadRequestException(
-          "Invalid password",
-          ErrorCode.INCORRECT_PASSWORD
-        )
-      );
-    }
-
-    const token = jwt.sign(
-      {
-        userId: user.id,
-      },
-      JWT_SECRET
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (!user) {
+    return next(
+      new NotFoundException("User not found", ErrorCode.USER_NOT_FOUND)
     );
-
-    res.json({ user, token });
-  } catch (err: any) {
-    next(err);
   }
+
+  if (!compareSync(password, user.password)) {
+    return next(
+      new BadRequestException("Invalid password", ErrorCode.INCORRECT_PASSWORD)
+    );
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    JWT_SECRET
+  );
+
+  res.json({ user, token });
+};
+
+export const me = async (req: Request, res: Response) => {
+  res.json(req.user);
 };
---
+++ src/middlewares/auth.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from "express";
+import { UnauthorizedException } from "../exceptions/unauthorized";
+import { ErrorCode } from "../exceptions/root";
+import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets";
+import { prismaClient } from "..";
+
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return next(
+      new UnauthorizedException(
+        "Token not found (Unauthorized)",
+        ErrorCode.UNAUTHORIZED
+      )
+    );
+  }
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET) as any;
+
+    const user = await prismaClient.user.findFirst({
+      where: { id: payload.userId },
+    });
+
+    if (!user) {
+      return next(
+        new UnauthorizedException(
+          "User not found (Unauthorized)",
+          ErrorCode.UNAUTHORIZED
+        )
+      );
+    }
+
+    req.user = user as any;
+    next();
+  } catch (error) {
+    return next(
+      new UnauthorizedException(
+        "Invalid token (Unauthorized)",
+        ErrorCode.UNAUTHORIZED
+      )
+    );
+  }
+};
+
+export default authMiddleware;
