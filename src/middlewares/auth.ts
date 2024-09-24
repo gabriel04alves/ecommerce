c7bc9d40eff6a905cccd8af0a1525c66eed8cff1
Gabriel Alves <gabriel52.alves@gmail.com>
2024-09-24 16:18:08 -0300

    fix: fixing authentication error

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -10,9 +10,11 @@ const authMiddleware = async (
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(" ")[1]; // Obtém apenas o token
 
   if (!token) {
+    console.log("Token not found (Unauthorized)");
     return next(
       new UnauthorizedException(
         "Token not found (Unauthorized)",
@@ -22,7 +24,7 @@ const authMiddleware = async (
   }
 
   try {
-    const payload = jwt.verify(token, JWT_SECRET) as any;
+    const payload = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
 
     const user = await prismaClient.user.findFirst({
       where: { id: payload.userId },
@@ -37,7 +39,7 @@ const authMiddleware = async (
       );
     }
 
-    req.user = user as any;
+    req.user = user;
     next();
   } catch (error) {
     return next(
