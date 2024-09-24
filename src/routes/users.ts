6e0c3055367c9621a37c902f37c88a66a9a6ae88
Gabriel Alves <gabriel52.alves@gmail.com>
2024-09-24 18:08:49 -0300

    Refactor user routes to require admin privileges for certain endpoints

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -10,6 +10,7 @@ import {
   listUsers,
   updateUser,
 } from "../controllers/users";
+import adminMiddleware from "../middlewares/admin";
 
 const usersRoutes: Router = Router();
 
@@ -21,8 +22,20 @@ usersRoutes.delete(
 );
 usersRoutes.get("/address", [authMiddleware], errorHandler(listAddress));
 usersRoutes.put("/", [authMiddleware], errorHandler(updateUser));
-usersRoutes.put("/:id/role", [authMiddleware], errorHandler(changeUserRole));
-usersRoutes.get("/", [authMiddleware], errorHandler(listUsers));
-usersRoutes.get("/:id", [authMiddleware], errorHandler(getUserById));
+usersRoutes.put(
+  "/:id/role",
+  [authMiddleware, adminMiddleware],
+  errorHandler(changeUserRole)
+);
+usersRoutes.get(
+  "/",
+  [authMiddleware, adminMiddleware],
+  errorHandler(listUsers)
+);
+usersRoutes.get(
+  "/:id",
+  [authMiddleware, adminMiddleware],
+  errorHandler(getUserById)
+);
 
 export default usersRoutes;
